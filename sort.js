4f53fd32977abfc59f303ca79e6f328cdf70b899
Lalit1223 <128039870+Lalit1223@users.noreply.github.com>
2024-07-22 23:00:28 +0530

    22-07

---
+++ sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// Example usage:
+let array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Unsorted array: " + array);
+console.log("Sorted array: " + bubbleSort(array));
